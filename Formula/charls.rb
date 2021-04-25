76c6db6a2c6393acdb103b73d3b43c4d54fbbbd3
Victor Derks <vbaderks@gmail.com>
2021-04-25 19:58:40 +0200

    Update description
    Default descrription includes Formula name, which is not allowed.

--- Formula/charls.rb
+++ Formula/charls.rb
@@ -1,5 +1,5 @@
 class Charls < Formula
-  desc "CharLS, a C++ JPEG-LS library implementation"
+  desc "C++ JPEG-LS library implementation"
   homepage "https://github.com/team-charls/charls"
   url "https://github.com/team-charls/charls/archive/2.2.0.tar.gz"
   sha256 "e1d7dd70cd9d6d46de5abbf22278dc8169707995a21e8bf705f5746c04c76891"
