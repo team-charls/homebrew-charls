7f8fe0374d6675f4a9c17c90fc3df50c6537da64
Victor Derks <vbaderks@gmail.com>
2021-05-06 23:51:26 +0200

    Add bottle do step
    Closes #5.
    
    Signed-off-by: Victor Derks <vbaderks@gmail.com>

--- Formula/charls.rb
+++ Formula/charls.rb
@@ -5,6 +5,10 @@ class Charls < Formula
   sha256 "e1d7dd70cd9d6d46de5abbf22278dc8169707995a21e8bf705f5746c04c76891"
   license "BSD-3-Clause"
 
+  bottle do
+    sha256 cellar: :any, big_sur: "895f797f5001d7a7d0680875c3c9d0f806a9c7b38a8b2bf3714ea239bb3f3fd1"
+  end
+
   depends_on "cmake" => :build
 
   def install
