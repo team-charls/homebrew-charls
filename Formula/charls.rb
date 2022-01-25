48e18473e74ba74b0b156a29329dbf1c756bd078
Victor Derks <vbaderks@gmail.com>
2022-01-26 00:28:08 +0100

    Update to CharLS v2.3.1 (#6)

--- Formula/charls.rb
+++ Formula/charls.rb
@@ -1,12 +1,12 @@
 class Charls < Formula
   desc "C++ JPEG-LS library implementation"
   homepage "https://github.com/team-charls/charls"
-  url "https://github.com/team-charls/charls/archive/2.2.0.tar.gz"
-  sha256 "e1d7dd70cd9d6d46de5abbf22278dc8169707995a21e8bf705f5746c04c76891"
+  url "https://github.com/team-charls/charls/archive/2.3.1.tar.gz"
+  sha256 "274107538c70abfce5459c40ed1ec5ef354193ad9e2c55de6293ca358d52e97"
   license "BSD-3-Clause"
 
   bottle do
-    root_url "https://github.com/team-charls/homebrew-tap/releases/download/charls-2.2.0"
+    root_url "https://github.com/team-charls/homebrew-tap/releases/download/charls-2.3.1"
     rebuild 1
     sha256 cellar: :any,                 catalina:     "cef510c039c7f062820704fe3a7b1611939c060e5685aa88ec7d44930520c062"
     sha256 cellar: :any_skip_relocation, x86_64_linux: "6d34c5f68a3745a2c75162e382bf794d8c1844ebfc6a61fdc97c0ae2bdd4a523"
--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # homebrew-tap
 
 Homebrew formulae that allow installation of Team-Charls projects through the [Homebrew](https://brew.sh/) package manager.
-At the moment the only supported project is CharLS v2.2.0.
+The only supported project is CharLS v2.3.1.
 
 ## Installation
 
